f6592e2888bbeda183e09fcb830f932befcf7250
mruchkina <ruchkina.retouch@gmail.com>
2023-12-19 05:55:59 +0300

    first commit

---
+++ src/main/java/examples/calculator/CalcLogger.java
@@ -0,0 +1,7 @@
+package examples.calculator;
+public class CalcLogger implements Loggable{
+    @Override
+    public void saveLog(String str) {
+        System.out.println("Log message: " + str);
+    }
+}
